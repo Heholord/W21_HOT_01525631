4a2c0ece9a4886b9956eff1d2c042a4443bc496f
Manuel Esberger <esberger.manuel@live.at>
2021-12-06 21:21:44 +0100

    finish excercise: add local search correlation coeff and scatter plot

--- 3helper.py
+++ 3helper.py
@@ -44,9 +44,11 @@ def get_clauses(i):
         not i['x3'] or not i['x4'],
     ]
 
-def neigborhood(interpretation):
+def neigborhood(interpretation, search_order = None):
+    if not search_order:
+        search_order = interpretation.keys()
     neig = []
-    for key in interpretation.keys():
+    for key in search_order:
         # new_interpretation = {
         #             'x1': interpretation['x1'],
         #             'x2': interpretation['x2'],
@@ -69,9 +71,20 @@ def is_local_opt(interpretation):
         pot_local_opt = count_sat(get_clauses(neighbor))
         # print(pot_local_opt)
         if pot_local_opt > local_opt:
-            # print("sss")
             return False
     return True
+
+
+def local_search(interpretation, search_order):
+    best_interpretation = interpretation
+    best_val = count_sat(get_clauses(interpretation))
+    nh = neigborhood(interpretation, search_order=search_order)
+    for neighbor in nh:
+        objective_value = count_sat(get_clauses(neighbor))
+        if objective_value > best_val:
+            return local_search(neighbor, search_order)
+    return best_interpretation
+
 
 print(f"x1 x2 x3 x4| (x1 v x2) ∧ (x1 v x3) ∧ (x1 v ~x3) ∧ (~x1 v x4) ∧ (~x2 v ~x3) ∧ (x2 v ~x4) ∧ (~x3 v ~x4) | nr of sat clauses | global | local")
 print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------")
@@ -79,3 +92,51 @@ for i in interpretations:
     clauses = get_clauses(i)
     str_for_clauses = "            ".join([v(c) for c in clauses])
     print(f"{v(i['x1'])}  {v(i['x2'])}  {v(i['x3'])}  {v(i['x4'])} |     {str_for_clauses}      |         {count_sat(clauses)}         |    {v(evaluate(clauses))}   |   {v(is_local_opt(i))}")
+
+
+def get_as_string(i):
+    return f"{v(i['x1'])}{v(i['x2'])}{v(i['x3'])}{v(i['x4'])}"
+
+search_order1  = ['x1', 'x2', 'x3', 'x4']
+search_order2  = ['x4', 'x3', 'x2', 'x1']
+for search_order in [search_order1, search_order2]:
+    count_local_optima = dict()
+    for i in interpretations:
+        loc_opt = local_search(i, search_order)
+        str_local_opt = get_as_string(loc_opt)
+        print("interp", get_as_string(i), "leads to", str_local_opt)
+        count_local_optima[str_local_opt] = 0 if not (str_local_opt in count_local_optima) else (count_local_optima[str_local_opt] +1)
+    print("for search_order", search_order, ":",count_local_optima)
+
+def calc_distance_to_global_opt(i):
+    global_opt = {
+                    'x1': True,
+                    'x2': True,
+                    'x3': False,
+                    'x4': True                
+                }
+    hamming_distance = 0
+    for key in global_opt.keys():
+        if global_opt[key] != i[key]: hamming_distance += 1
+    return hamming_distance
+
+correlation = []
+for i in interpretations:
+    print("distance of", get_as_string(i), "to global opt", calc_distance_to_global_opt(i), "with a objective value of", count_sat(get_clauses(i))) 
+    correlation.append((count_sat(get_clauses(i)), calc_distance_to_global_opt(i)))
+
+import numpy as np
+objective= np.array([i[0] for i in correlation])
+distances= np.array([i[1] for i in correlation])
+corr = np.corrcoef(objective, distances)
+print(corr)
+
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dataFrame = pd.DataFrame({"objective value":objective, "distances":distances})
+# dataFrame.plot("objective value", "distances", kind='scatter')
+sns.regplot(data=dataFrame, x="distances", y="objective value")
+plt.show()
+
